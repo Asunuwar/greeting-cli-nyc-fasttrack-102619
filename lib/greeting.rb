97d0f857657d6c8ed27a09bb536ab76df0723ac2
Arun Sunuwar <sunuwar.arun@gmail.com>
2019-11-27 14:29:00 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,15 @@
+
+#sally, "bob       "
+def gretting(name="katharine")
+  puts "Hello #{name.strip}. It's nice to meet you."
+ end
+
+
+
+
+learnspec/01_greeting_spec.RUBY_VERSION
+
+  
+
+
 # code the #greeting method here!
